abde1bbfc0662193c940963c5bff7941f5b2c4a8
Alya <alyai@users.noreply.github.com>
2019-10-24 01:37:26 -0500

    Update app-deploy-script.sh

--- app-deploy-script.sh
+++ app-deploy-script.sh
@@ -4,10 +4,7 @@
 # creates new build of ASP.NET Core Web App, creating a new release and pushing to prduction on Azure App Service. 
 # The source code is hosted in GitHub Repository. When the source code is updated and pushed to GitHub Azure DevOps
 # automatically deploy it to production.
- 
 
-# Login to Azure account. Will prompt for password.
-#read -sp "Azure password: " AZ_PASS && echo && az login -u gmgold@gmail.com -p $AZ_PASS
 
 az extension add --name "azure-devops"
 sleep 5
@@ -17,7 +14,7 @@ az devops login --organization https://dev.azure.com/iqwebapp/
 read -p "Connecting to Azure DevOps account..." -t 6
 
 # Running the build on Azure DevOps from the source code on GitHub, deploying the release to production.
-az pipelines build queue --definition-name "webapp-challenge-app - CI" --project webapp-challenge-project -o table
+az pipelines build queue --definition-name "webapp-challenge-app - CI" --project webapp-challenge-project
 sleep 7
 read -p "Creating the build..." -t 40
 echo "Done!"
