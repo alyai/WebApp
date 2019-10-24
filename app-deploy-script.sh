8523a46b8be8e2ec0981caa570116ae1b3b6d4e1
Alya <alyai@users.noreply.github.com>
2019-10-23 22:48:28 -0500

    Version 2.1

--- app-deploy-script.sh
+++ app-deploy-script.sh
@@ -1,46 +1,54 @@
 #!/bin/bash
 
-# This script connects to Azure Cloud account - you need to enter the password when prompted
-#
+# This script connects to Azure Cloud account (you need to enter the password ans token when prompted) and
+# creates new build of ASP.NET Core Web App, creating a new release and pushing to prduction on Azure App Service. 
+# The source code is hosted in GitHub Repository. When the source code is updated and pushed to GitHub Azure DevOps
+# automatically deploy it to production.
+ 
 
-
+# Login to Azure account. Will prompt for password.
 read -sp "Azure password: " AZ_PASS && echo && az login -u gmgold@gmail.com -p $AZ_PASS
 
 az extension add --name "azure-devops"
 sleep 5
 
+# Login to Azure DevOps. Will prompt for token.
 az devops login --organization https://dev.azure.com/iqwebapp/
 read -p "Connecting to Azure DevOps account..." -t 6
 
-
+# Running the build on Azure DevOps from the source code on GitHub, deploying the release to production.
 az pipelines build queue --definition-name "webapp-challenge-app - CI" --project webapp-challenge-project -o table
+wait 3
 read -p "Creating the build..." -t 40
 echo "Done!"
 
+# Show the list of all builds done with statuses.
 echo "Showing the list of all builds..."
 sleep 10
 az pipelines build list --definition-ids 1 --project webapp-challenge-project -o table
 sleep 5
 echo "Done!"
 
+# Displaying informative message. 
 read -p "Deploying the latest release..." -t 30
 echo "Showing the list of all releases..."
 sleep 6
 
+# Show the list of all releases with their statuses.
 az pipelines release list --definition-id 1 --project webapp-challenge-project -o table
 sleep 10
 echo "Done!"
 sleep 5
 
-
+# Web App URL.
 webappname=https://webapp-challenge-app.azurewebsites.net
 
-# Web App URL.
+# Dispaly the Web App URL
 echo "WebApp URL:"
 echo $webappname
 
 sleep 5
 read -p "WebApp will be opened in browser now..." -t 3
 
-# Open URL in browser
+# Open Web App URL in browser
 az webapp browse --name webapp-challenge-app --resource-group webapp-challenge-app-rg
